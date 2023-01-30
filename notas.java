e38b156a079ce9cf784776091e4918b7a7bb6b2e
TÉCNICO EM INFORMÁTICA <51127512022.2@senacrjedu.local>
2023-01-30 12:12:58 -0300

    arquivos da aula de segunda dia 30/01

--- notas.java
+++ notas.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class index{
+public class notas {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        double nota1, nota2, nota3, media;
+        double nota1, nota2, nota3, novaNota, media;
 
         System.out.println("Informe a primeira nota");
         nota1 = sc.nextDouble();
@@ -18,5 +18,13 @@ public class index{
         media = (nota1 + nota2 + nota3) / 3;
 
         System.out.println("A média das notas é:" + media);
+    
+        System.out.println("adicionar mais uma nota?");
+if{
+        System.out.println("Informe mais uma nota");
+        novaNota = sc.nextDouble();
+} 
+
+//colocar um sim ou não E um contator de variavel acrescentada
     }
 }
