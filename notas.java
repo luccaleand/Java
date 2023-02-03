5afcec0d4d13fd3d4e098e09aa8285b46f24ac45
TÉCNICO EM INFORMÁTICA <51127512022.2@senacrjedu.local>
2023-02-03 11:56:00 -0300

    aula03/02

--- notas.java
+++ notas.java
@@ -4,7 +4,7 @@ public class notas {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        double nota1, nota2, nota3, novaNota, media;
+        double nota1, nota2, nota3, maisNota, media;
 
         System.out.println("Informe a primeira nota");
         nota1 = sc.nextDouble();
@@ -20,10 +20,14 @@ public class notas {
         System.out.println("A média das notas é:" + media);
     
         System.out.println("adicionar mais uma nota?");
-if{
-        System.out.println("Informe mais uma nota");
-        novaNota = sc.nextDouble();
-} 
+        
+
+    } if (expressão booleana) {
+    System.out.println("Informe mais uma nota");
+    maisNota = sc.nextDouble();
+    } else {
+    // bloco de código 2
+}
 
 //colocar um sim ou não E um contator de variavel acrescentada
     }
